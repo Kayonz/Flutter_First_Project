2d8b66a23194ad12efb4a9f8a114dd4bef496a9e
VITOR KAYON DE LARA MADRUGA <vitor_k_madruga@estudante.sesisenai.org.br>
2023-08-23 20:25:18 -0300

    Flutte_teste

---
+++ lib/Apps/segunda_tela.dart
@@ -0,0 +1,184 @@
+import 'package:teste_flutter/Apps/terceira_tela.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+import 'home.dart';
+
+class SegundaTela extends StatefulWidget {
+  const SegundaTela({super.key, required this.title});
+  final String title;
+  @override
+  State<SegundaTela> createState() => _SegundaTelaState();
+}
+
+class _SegundaTelaState extends State<SegundaTela> {
+  final _value1 = TextEditingController();
+  final _value2 = TextEditingController();
+
+  double result = 0;
+
+  void Sum(){
+    setState(() {
+      result = (double.tryParse(_value1.text) ?? 0.0) + (double.tryParse(_value2.text) ?? 0.0);
+    });
+  }
+
+  void Sub(){
+    setState(() {
+      result = (double.tryParse(_value1.text) ?? 0.0) - (double.tryParse(_value2.text) ?? 0.0);
+    });
+  }
+
+  void Div(){
+    setState(() {
+      result = (double.tryParse(_value1.text) ?? 0.0) / (double.tryParse(_value2.text) ?? 0.0);
+    });
+  }
+
+  void Mult(){
+    setState(() {
+      result = (double.tryParse(_value1.text) ?? 0.0) * (double.tryParse(_value2.text) ?? 0.0);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child:  ListView(
+          //teste de comentario
+          //padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+
+            ListTile(
+              title: const Text('Bem Vindo!'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyHomePage(title: "Seja Bem-vindo")),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Calculadora'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const TerceiraTela(title: "Calculadora")),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Formulário'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const TerceiraTela(title: "Formulário")),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            //Valor 1
+            TextFormField(
+              controller: _value1,
+              decoration: const InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          width: 1,
+                          color: Colors.white60
+                      )
+                  ),
+                  hintText: "First number",
+                  icon: Icon(Icons.numbers, color: Colors.lightBlueAccent,)
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 10,),
+            //Valor 2
+            TextFormField(
+              controller: _value2,
+              decoration: const InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          width: 1,
+                          color: Colors.white60
+                      )
+                  ),
+                  hintText: "Second number",
+                  icon: Icon(Icons.numbers, color: Colors.lightBlueAccent,)
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 20,),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                  ElevatedButton(onPressed: () {
+                    Sum();
+                    SizedBox(
+                      height: 10,
+                      width: 10,
+                    );
+                  },
+                      child: const Text("Sum",
+                          style: TextStyle(color: Colors.black,
+                              fontSize: 20))),
+                  SizedBox(width: 10,),
+
+                  ElevatedButton(onPressed: () {
+                    Div();
+                  },
+                      child: const Text("Divide",
+                          style: TextStyle(color: Colors.black,
+                              fontSize: 20))),
+
+                ]
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: () {
+                  Sub();
+                },
+                    child: const Text("Subtract",
+                        style: TextStyle(color: Colors.black,
+                            fontSize: 20))),
+                SizedBox(width: 10,),
+
+                ElevatedButton(onPressed: () {
+                  Mult();
+                },
+                    child: const Text("Multiply",
+                        style: TextStyle(color: Colors.black,
+                            fontSize: 20))),
+                SizedBox(height: 10,),
+              ],
+            ),
+            SizedBox(height: 20,),
+            Text("Resultado: $result", style: TextStyle(color: Colors.red, fontSize: 30))
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
